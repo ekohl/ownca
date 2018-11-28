5721d312b8892105d1b2db2804f12c2a8358e9ca
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-28 17:45:04 +0100

    Do not modify openssl.cnf

--- generate-ca.sh
+++ generate-ca.sh
@@ -6,4 +6,3 @@ mkdir -p private/
 
 openssl req -new -x509 -extensions v3_ca -keyout \
 private/cakey.crt -out cacert.crt -days 3650 -config ./openssl.cnf
-echo "subjectAltName                          = @alt_names" >> ./openssl.cnf
