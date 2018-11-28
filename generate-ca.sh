a093fb27a15e42ba9921db20a2cbdebcece4ff12
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-11-28 18:07:47 +0100

    Do not require a passphrase on the CA

--- generate-ca.sh
+++ generate-ca.sh
@@ -4,5 +4,5 @@ touch certindex.txt
 mkdir -p certs/
 mkdir -p private/
 
-openssl req -new -x509 -extensions v3_ca -keyout \
-private/cakey.crt -out cacert.crt -days 3650 -config ./openssl.cnf
+openssl req -new -x509 -extensions v3_ca -nodes -keyout private/cakey.crt \
+	-out cacert.crt -days 3650 -config ./openssl.cnf
