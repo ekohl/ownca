6b8362cb6b1630af5c1769445780f6163e1c702c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-12-03 15:06:55 +0100

    Export CERT_HOST to avoid an openssl error
    error on line 78 of ./openssl.cnf
    140546353997712:error:0E065068:configuration file routines:STR_COPY:variable has no value:conf_def.c:584:line 78

--- generate-ca.sh
+++ generate-ca.sh
@@ -4,5 +4,6 @@ touch certindex.txt
 mkdir -p certs/
 mkdir -p private/
 
+export CERT_HOST=""
 openssl req -new -x509 -extensions v3_ca -nodes -keyout private/cakey.crt \
 	-out cacert.crt -days 3650 -config ./openssl.cnf
