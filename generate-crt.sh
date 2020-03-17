088bb48fe5c83da1b428182d4c7c3544ec2eca65
Stephen Wadeley <swadeley@redhat.com>
2020-03-17 15:23:56 +0000

    Typos and remove whitespace

--- generate-crt.sh
+++ generate-crt.sh
@@ -10,11 +10,6 @@ mkdir $CERT_HOST
 cat <<EOF | openssl req -new -nodes -out ./$CERT_HOST/$CERT_HOST.crt.req\
   -keyout ./$CERT_HOST/$CERT_HOST.key -config ./openssl.cnf
 
-
-
-
-
-
 $CERT_HOST
 EOF
 
