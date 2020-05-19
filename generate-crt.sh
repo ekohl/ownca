1ad274a312de0b86cc14c3979eaffbbf01800902
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-19 17:57:20 +0200

    Use openssl req non-interactive
    This was the easiest way for me to make it work on EL8.

--- generate-crt.sh
+++ generate-crt.sh
@@ -7,10 +7,10 @@ if [[ -z $CERT_HOST ]] ; then
 fi
 
 mkdir $CERT_HOST
-cat <<EOF | openssl req -new -nodes -out ./$CERT_HOST/$CERT_HOST.crt.req\
-  -keyout ./$CERT_HOST/$CERT_HOST.key -config ./openssl.cnf
-
-$CERT_HOST
-EOF
+openssl req -new -nodes \
+	-subj "/CN=$CERT_HOST" \
+	-addext "subjectAltName = DNS:$CERT_HOST" \
+	-out ./$CERT_HOST/$CERT_HOST.crt.req\
+	-keyout ./$CERT_HOST/$CERT_HOST.key
 
 openssl ca -out ./$CERT_HOST/$CERT_HOST.crt -config ./openssl.cnf -infiles ./$CERT_HOST/$CERT_HOST.crt.req
