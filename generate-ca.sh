a498823b23eeed7db9804f1bf72eb3760018544e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-22 12:10:08 +0200

    Remove serial from git and auto generate

--- generate-ca.sh
+++ generate-ca.sh
@@ -6,6 +6,10 @@ touch certindex.txt
 mkdir -p certs/
 mkdir -p private/
 
+if [[ ! -e serial ]] ; then
+        echo 100001 > serial
+fi
+
 openssl req -batch -new -x509 -config ./openssl.cnf \
 	-subj "$SUBJECT" -extensions v3_ca -nodes \
 	-keyout private/cakey.crt \
